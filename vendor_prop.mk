0b4e22e591b8b3f1fefc922fef46c9201c571ce6
Shashank Verma <shashank.verma2002@gmail.com>
2020-08-06 07:42:38 +0800

    Add extra necessary props
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -5,3 +5,35 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.block_allow_data=1
+
+# Apex
+PRODUCT_PROPERTY_OVERRIDES += ro.apex.updatable=true
+
+# Assistant
+PRODUCT_PROPERTY_OVERRIDES += ro.opa.eligible_device=true
+
+# Camera Test
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.camera.awb.sync=2 \
+    persist.vendor.camera.expose.aux=1 \
+    persist.vendor.camera.imglib.usefdlite=1 \
+    vendor.camera.aux.packagelist=org.codeaurora.snapcam \
+    vendor.camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
+    persist.vendor.camera.tnr.cds=1 \
+    persist.vendor.process.plates=1 \
+    persist.vendor.process.plates=1 \
+    persist.vendor.camera.eis.enable=1 \
+    persist.vendor.camera.HAL3.enabled=1 \
+    persist.vendor.camera.is_type=5 \
+    persist.vendor.camera.is_mode=5 \
+    persist.vendor.camera.max.previewfps=60 \
+    persist.vendor.camera.tnr_cds=1 \
+    persist.vendor.camera.tnr.video=1 \
+    persist.vendor.camera.tnr.preview=1 \
+    persist.vendor.camera.tnr.snapshot=1 \
+    persist.vendor.camera.llnoise=1 \
+    persist.vendor.tnr.process.plates=2 \
+    persist.vendor.denoise.process.plates=2 \
+    vendor.camera.lowpower.record.enable=1 \
+    persist.vendor.camera.rdi.mode=1 \
+    persist.vendor.camera.video.CDS=1
