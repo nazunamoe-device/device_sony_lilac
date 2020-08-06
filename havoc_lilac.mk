77ae9b23b301fdac7c22c20869f567e0d1d074e8
Shashank Verma <shashank.verma2002@gmail.com>
2020-08-06 08:02:08 +0800

    WITH_VOLTE should be false by default
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- havoc_lilac.mk
+++ havoc_lilac.mk
@@ -2,6 +2,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+WITH_VOLTE := false
+
 # Inherit device configuration
 $(call inherit-product, device/sony/lilac/device.mk)
 
