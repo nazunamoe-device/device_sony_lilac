a6d03a9e42bc9dad39847343f6bb042dfe8ebf12
Shashank <shashank.verma2002@gmail.com>
2020-08-11 16:52:39 +0800

    patches: Clean-up and add mem fix
    Remove un-necessary patches and add patch fix for camera memory leak
    
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- patches/applyPatches.sh
+++ patches/applyPatches.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-cd ..
-cd ..
-cd ..
-cd ..
+cd .. && cd .. && cd .. && cd ..
 cp device/sony/lilac/patches/assistant.patch frameworks/base
 cd frameworks/base
 
@@ -13,29 +10,18 @@ git revert 7d7bebd4bd58149b10e3b44fb9c559d25762d9fd
 # Apply assistant uid patch
 git apply assistant.patch
 
-cd ..
-cd ..
+cd .. && cd ..
 cd packages/apps/Settings
 
 # Enable GooglePlaySystemUpdate
 git revert fcca4fa9da67d3409df9729dde0c14c60f831af5
 
-cd ..
-cd ..
-cd ..
-cp device/sony/lilac/patches/gpsu.patch vendor/havoc
-cd vendor/havoc
+cd .. && cd .. && cd ..
+cp device/sony/lilac/patches/mem.patch hardware/qcom-caf/msm8998/display
+cd hardware/qcom-caf/msm8998/display
 
-# Enable GooglePlaySystemUpdate overlay
-git apply gpsu.patch
+# Fix camera mem leak
+git apply mem.patch
 
-cd ..
-cd ..
-cp device/sony/lilac/patches/build.patch build/make
-cd build/make
-
-# Build tweaks
-git apply build.patch
-
-cd .. && cd ..
+cd .. && cd .. && cd .. && cd ..
 echo "Done."
