6c4d2585d73f9f56ab4a3c5cdbec4ce1d577e2a8
Shashank Verma <shashank.verma2002@gmail.com>
2021-01-14 10:01:10 +0800

    lilac: Update pixel fp to January
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- havoc_lilac.mk
+++ havoc_lilac.mk
@@ -47,11 +47,11 @@ BUILD_VERSION_TAGS := release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=lilac \
-    PRIVATE_BUILD_DESC="redfin-user 11 RD1A.201105.003.A1 6886512 release-keys" \
-    BUILD_NUMBER=6886512
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.210105.003 7005429 release-keys" \
+    BUILD_NUMBER=7005429
 
-# Pixel 5 November fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RD1A.201105.003.A1/6886512:user/release-keys
+# Pixel 5 January fingerprint
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
 #BUILD_FINGERPRINT := Sony/G8441/G8441:9/47.2.A.11.228/3311891731:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-sony-mobile
